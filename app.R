3e60b6014318b284809cb487a1a18163a4b6db70
unknown <cfleit@LIN-L-427902.PFR.CO.NZ>
2016-01-19 17:43:28 +1300

    First prototype to read and display CCII data

---
+++ app.R
@@ -0,0 +1,63 @@
+# 01-kmeans-app
+
+palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+  "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))
+
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+allData <- read.csv("C:\\ShinyR_tutorial\\study1\\AllData(RA2).csv", header = TRUE)
+
+ui <- fluidPage(
+  headerPanel('RA2 CCII Arable crop'),
+  sidebarPanel(
+    selectInput('scn', 'Select the crop', as.character(unique(allData$thisScenario))),
+    selectInput('crop', 'Select the crop', as.character(unique(allData$CurrentCrop))),
+    selectInput('soil', 'Select the soil', as.character(unique(allData$thisSoil))),
+    selectInput('xcol', 'Select X Variable', names(allData)),
+    selectInput('ycol', 'Select Y Variable', names(allData),
+      selected = names(allData)[[12]]),
+    numericInput('clusters', 'Cluster count', 3,
+      min = 1, max = 9)
+  ),
+  mainPanel(
+    plotOutput('plot1')
+  )
+)
+
+server <- function(input, output) {
+
+  selectedData <- reactive({
+    
+    # Due to dplyr issue #318, we need temp variables for input values
+    crop <- input$crop
+    soil <- input$soil
+    scn <- input$scn
+    
+    allData <- allData %>%
+      filter( CurrentCrop == crop & 
+                thisSoil == soil  &
+               thisScenario == scn
+              )
+    
+    
+    allData[, c(input$xcol, input$ycol)]
+  })
+
+  clusters <- reactive({
+    kmeans(selectedData(), input$clusters)
+  })
+
+  output$plot1 <- renderPlot({
+    par(mar = c(5.1, 4.1, 0, 1))
+    plot(selectedData(),
+         col = clusters()$cluster,
+         pch = 20, cex = 3)
+    points(clusters()$centers, pch = 4, cex = 4, lwd = 4) 
+    
+    })
+
+}
+
+shinyApp(ui = ui, server = server)
